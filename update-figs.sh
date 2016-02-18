3471b0a74c885a2ce20999761b9440856dd1fbe4
MarekSchoenherr <marek.schoenherr@cern.ch>
2016-02-18 23:57:34 +0100

    update and clarifications for MG added figs as discussed with simon updated figs, a bit more legible, removed most histos where not applicable

--- update-figs.sh
+++ update-figs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-OBS_H="H_y H_pT_incl HT_jets NJet_incl_30 NJet_excl_30"
-OBS_HJ="H_j_pT_incl Hj_pT_incl"
-OBS_HJJ="H_jj_pT_incl H_dijet_mass"
-OBS_HJJJ="H_jjj_pT_incl Hjjj_pT_incl"
+OBS_H="H_y H_pT_incl H_pT_excl NJet_incl_30 NJet_excl_30"
+OBS_HJ="H_j_pT_incl HT_jets jet1_pT_incl jet1_y Hj_pT_incl Hj_pT_excl"
+OBS_HJJ="H_jj_pT_incl jet2_pT_incl dijet_mass deltay_jj"
+OBS_HJJJ="H_jjj_pT_incl jet3_pT_incl"
 OBS_VBF=""
-OBS_EXCL="H_pT_excl Hj_pT_excl"
+OBS_EXCL=""
 OBS_JV="xs_central_jet_veto_VBF xs_jet_veto_j0 xs_jet_veto_j1_100 xs_jet_veto_h_100"
 
 if [ -d /home/marek/work/LH15/Sherpa-GoSamComparison ];
